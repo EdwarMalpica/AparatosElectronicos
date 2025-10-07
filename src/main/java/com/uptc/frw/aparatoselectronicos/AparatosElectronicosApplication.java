c92f57555420428bc72ee20e909a9ea082ea0b31
EdwarMalpica <edwar.malpica@uptc.edu.co>
2025-10-06 21:20:40 -0500

    Initial commit

---
+++ src/main/java/com/uptc/frw/aparatoselectronicos/AparatosElectronicosApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.aparatoselectronicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AparatosElectronicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AparatosElectronicosApplication.class, args);
+    }
+
+}
