80bf697c0ec4bd10ef0abb7378ab407e325d30e5
David Leonardo Malaver Valenzuela <david.malaver@bemyself.com>
2025-10-08 23:15:16 -0500

    Repair model, repository, service and controller added

---
+++ src/main/java/com/uptc/frw/aparatoselectronicos/controller/RepairController.java
@@ -0,0 +1,41 @@
+package com.uptc.frw.aparatoselectronicos.controller;
+
+import com.uptc.frw.aparatoselectronicos.model.Repair;
+import com.uptc.frw.aparatoselectronicos.service.RepairService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping (path = "repairs")
+public class RepairController {
+
+    @Autowired
+    private RepairService repairService;
+
+    @GetMapping
+    public List<Repair> getRepairs() {
+        return repairService.findAllRepairs();
+    }
+
+    @GetMapping ("/{id}")
+    public  Repair getClients(@PathVariable Long id) {
+        return repairService.findRepairById(id);
+    }
+
+    @PostMapping
+    public Repair createRepair(@RequestBody Repair repair) {
+        return repairService.saveRepair(repair);
+    }
+
+    @PutMapping
+    public Repair updateRepair(@RequestBody Repair repair) {
+        return repairService.updateRepair(repair);
+    }
+
+    @DeleteMapping ("/{id}")
+    public void deleteRepair(@PathVariable Long id) {
+        repairService.deleteReapir(id);
+    }
+}
