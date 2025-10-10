44c9f33d2bfc035794022833ccdb556f0b999283
David Fajardo <nidafa202426@gmail.com>
2025-10-09 20:20:19 -0500

    feat: Implements Components CRUD

---
+++ src/main/java/com/uptc/frw/aparatoselectronicos/controller/ComponentController.java
@@ -0,0 +1,40 @@
+package com.uptc.frw.aparatoselectronicos.controller;
+
+import com.uptc.frw.aparatoselectronicos.model.Component;
+import com.uptc.frw.aparatoselectronicos.service.ComponentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("components")
+public class ComponentController {
+    @Autowired
+    private ComponentService componentService;
+
+    @GetMapping
+    public List<Component> getAllComponents() {
+        return componentService.findAllComponents();
+    }
+
+    @GetMapping ("/{id}")
+    public  Component getComponent(@PathVariable Long id) {
+        return componentService.findComponentById(id);
+    }
+
+    @PostMapping
+    public Component createComponent(@RequestBody Component component) {
+        return componentService.saveComponent(component);
+    }
+
+    @PutMapping
+    public Component updateComponent(@RequestBody Component component) {
+        return componentService.updateComponent(component);
+    }
+
+    @DeleteMapping ("/{id}")
+    public void deleteComponent(@PathVariable Long id) {
+        componentService.deleteComponent(id);
+    }
+}
---
+++ src/main/java/com/uptc/frw/aparatoselectronicos/service/ComponentService.java
@@ -0,0 +1,31 @@
+package com.uptc.frw.aparatoselectronicos.service;
+
+import com.uptc.frw.aparatoselectronicos.model.Component;
+import com.uptc.frw.aparatoselectronicos.repository.ComponentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ComponentService {
+    @Autowired
+    ComponentRepository componentRepository;
+
+    public List<Component> findAllComponents() { return componentRepository.findAll(); }
+
+    public Component findComponentById(long id){ return componentRepository.findById(id).orElse(null); }
+
+    public Component saveComponent(Component component){ return componentRepository.save(component);}
+
+    public Component updateComponent(Component component){
+        Component componentToUpdate = findComponentById(component.getId());
+        if(componentToUpdate == null) throw new RuntimeException("Registro no encontrado");
+
+        componentToUpdate.setName(component.getName());
+        componentToUpdate.setSpecs(component.getSpecs());
+        return saveComponent(componentToUpdate);
+    }
+
+    public void deleteComponent(long id){ componentRepository.deleteById(id); }
+}
